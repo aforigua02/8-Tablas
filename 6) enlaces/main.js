891fb009bc6eda8d6d1f0f95f54edaa1f2032978
aforigua02 <pipeforigua@gmail.com>
2022-07-22 13:07:17 -0500

    Initial commit

---
+++ 6) enlaces/main.js
@@ -0,0 +1,8 @@
+'use strict'
+//los contenedores que posee JS
+var variable1=3; //ámbito global
+let variable2=4; //ámbito local 
+const variable=1; //no se modifica
+
+console.log(variable1+variable2);
+//alert("EL valor de la variable2:"+variable2);
